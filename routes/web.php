ac2036aa3e5990b048c62f9b7492c80f6e58158a
fernandoDC <fernando@dcircuits.mx>
2020-11-12 01:12:04 -0600

    Controlador, Vistas, y Migración agregadas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Pieza
+Route::get('/piezas','PiezasController@index'); //Muestra todas las piezas.
+
